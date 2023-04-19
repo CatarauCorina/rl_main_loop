a17f9ba7c89728e9eb8836defab1524a733e4832
CatarauCorina <corina_catarau@yahoo.ro>
2023-04-19 14:52:08 +0300

    run change

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ pip install virtualenv
 chmod +x /content/drive/MyDrive/rl_main_loop/rl_main_loop/bin/pip
 chmod +x /content/drive/MyDrive/rl_main_loop/rl_main_loop/bin/python
 source /content/drive/MyDrive/rl_main_loop/rl_main_loop/bin/activate || virtualenv rl_main_loop --python=python3.8
+cat requirements_main_lopp.txt
 pip install -r requirements_main_lopp.txt
 pip -V;
 python --version
