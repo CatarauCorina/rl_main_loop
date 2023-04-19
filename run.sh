d8b37dc471585745c025e13c06a046d3f22a4945
CatarauCorina <corina_catarau@yahoo.ro>
2023-04-19 15:32:02 +0300

    Remove animalai from requ

--- run.sh
+++ run.sh
@@ -5,9 +5,8 @@ pip install virtualenv
 chmod +x /content/drive/MyDrive/rl_main_loop/rl_main_loop/bin/pip
 chmod +x /content/drive/MyDrive/rl_main_loop/rl_main_loop/bin/python
 source /content/drive/MyDrive/rl_main_loop/rl_main_loop/bin/activate || virtualenv rl_main_loop --python=python3.8
-cat requirements_main_lopp.txt
 pip install -r requirements_main_lopp.txt
-pip -V;
+pip -V
 python --version
 pip install seaborn
 pip install scipy==1.9.3
