13f7f9a0bad62132701cabe43e392ec363d69616
CatarauCorina <corina_catarau@yahoo.ro>
2023-05-01 14:14:46 +0300

    fix imports

---
+++ animalai/animalai/envs/__init__.py
@@ -0,0 +1,5 @@
+import animalai.animalai.envs.actions
+import animalai.animalai.envs.braitenberg
+import animalai.animalai.envs.environment
+import animalai.animalai.envs.raycastparser
+import animalai.animalai.envs.settings
